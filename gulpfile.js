5d2f00dc34d3552aee5d8727684d20948c3c08b7
Julian_Chu <walkingice@0xlab.org>
2016-02-06 00:01:12 +0800

    build: involve webpack into gulp
    To use webpack-dev-server to privode a simple http server for
    development.
    
        $ npm start

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 'use strict';
 
+var path = require('path');
+
 var gulp = require('gulp');
+var gutil = require("gulp-util");
+var webpack = require("webpack");
+var WebpackDevServer = require("webpack-dev-server");
 
 // load plugins
 var $ = require('gulp-load-plugins')();
@@ -15,6 +20,32 @@ gulp.task('jade', function () {
         .pipe(gulp.dest(TMP + '/'));
 });
 
+gulp.task("webpack-dev-server", function() {
+    // https://webpack.github.io/docs/configuration.html
+    var compiler = webpack({
+        context: __dirname + '/app',
+        debug: true,
+        devtool: "#inline-source-map",
+        output: {
+            path: path.resolve(__dirname, "_tmp")
+        }
+    });
+
+    // https://github.com/webpack/docs/wiki/webpack-dev-server
+    new WebpackDevServer(compiler, {
+        contentBase: TMP,
+        hot: true,
+        inline: true,
+        progress: true,
+        stats: {
+            color: true
+        }
+    }).listen(PORT, "localhost", function(err) {
+        if(err) throw new gutil.PluginError("webpack-dev-server", err);
+        gutil.log("[webpack-dev-server]", "Run on http://localhost:" + PORT);
+    });
+});
+
 gulp.task('clean', function () {
     return gulp.src([TMP], { read: false }).pipe($.rimraf());
 });
@@ -25,6 +56,7 @@ gulp.task('default', ['clean'], function () {
 
 gulp.task('dev', ['jade'], function () {
     gulp.watch('app/**/*.jade', ['jade']);
+    gulp.start('webpack-dev-server');
 });
 
 
