f10281eda960388b133e2192dbdc154d1242c838
Julian_Chu <walkingice@0xlab.org>
2016-02-06 11:23:55 +0800

    react: involve react-router
    add some dummy view to validate router function

--- app/js/index.jsx
+++ app/js/index.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import {Router, Route, Redirect} from 'react-router'
+
+import App from './comps/App.jsx';
+import Lists from './comps/Lists.jsx';
+import Detail from './comps/Detail.jsx';
+
+const routes = <Route component={App}>
+  <Route path="/" component={Lists}/>
+  <Route path="/detail" component={Detail} />
+  <Redirect from="*" to="/"/>
+</Route>
 
 ReactDom.render(
-  <div>Render by react</div>,
+  <Router>
+    {routes}
+  </Router>,
   document.getElementById('app')
 );
