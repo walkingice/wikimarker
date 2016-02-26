4845b943017047b6148f44d32c27ecd42933f3af
Julian_Chu <walkingice@0xlab.org>
2016-02-26 17:54:49 +0800

    webpack: remove react-hot-loader
    react-hot-loader was deprecated. Actually we do not need it since
    webpack support the feature natively.

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015']
+                loaders: ['babel?presets[]=react,presets[]=es2015']
             }, {
                 test: /\.json$/,
                 loader: 'json'
